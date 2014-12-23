7cd0b3cf0d54c098a4f4e73fc4677cb47f23ed0d
Andy Casey <andycasey@gmail.com>
2014-12-23 23:07:13 +1100

    test

---
+++ status.rb
@@ -0,0 +1,9 @@
+require 'octokit'
+require 'yaml'
+
+#@client = Octokit::Client.new(:access_token => ENV['GH_TOKEN'])
+
+# This should just look for some status information from the science script
+# and create a GitHub status.
+
+# The 
