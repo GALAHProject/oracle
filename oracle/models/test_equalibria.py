cd6d37b30fc994e382d54ceb625a91e5ee6e02b0
Andy Casey <andycasey@gmail.com>
2014-12-27 23:00:25 +1100

    Don't run the equalibria test on Travis until online data hosting is solved

--- oracle/models/test_equalibria.py
+++ oracle/models/test_equalibria.py
@@ -1,25 +1,32 @@
 
+import os
 import oracle
 
-data = [oracle.specutils.Spectrum1D.load("Spectra/ccd_1/sun_blu.0002.fits")]
 
-model = oracle.models.EqualibriaModel({
-        "model": {
-            "redshift": True,
-            "instrumental_resolution": True,
-            "continuum": 2,
-            "atomic_lines": [
-                # wavelength, species, chi, loggf, damp1, damp2, synthesise_surrounding, opacity_contribution
-                [4800.648, 26.0, 4.120, -1.028, 0, 0, 1.0],
-                [4871.318, 26.0, 2.870, -0.362]
-            ]
-#            "atomic_lines_filename": "test_atomic_lines.txt"
-#            "blending_lines_filename": "test_blen"
-        },
-        "settings": {
-            "threads": 4
-        }
-    })
+def equalibria():
+    data = [oracle.specutils.Spectrum1D.load("Spectra/ccd_1/sun_blu.0002.fits")]
+
+    model = oracle.models.EqualibriaModel({
+            "model": {
+                "redshift": True,
+                "instrumental_resolution": True,
+                "continuum": 2,
+                "atomic_lines": [
+                    # wavelength, species, chi, loggf, damp1, damp2, synthesise_surrounding, opacity_contribution
+                    [4800.648, 26.0, 4.120, -1.028, 0, 0, 1.0],
+                    [4871.318, 26.0, 2.870, -0.362]
+                ]
+    #            "atomic_lines_filename": "test_atomic_lines.txt"
+    #            "blending_lines_filename": "test_blen"
+            },
+            "settings": {
+                "threads": 4
+            }
+        })
 
 
-theta = model.initial_theta(data)
+    theta = model.initial_theta(data)
+
+# Don't run this test on Travis.
+if "TRAVIS_BUILD_ID" not in os.environ:
+    equalibria()
