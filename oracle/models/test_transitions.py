8e158af87884ce5cd66ae9ad98ecebc63137aaee
Andy Casey <andycasey@gmail.com>
2015-01-04 15:39:33 +0000

    Updated teest script for transitions.py

--- oracle/models/test_transitions.py
+++ oracle/models/test_transitions.py
@@ -1,43 +1,59 @@
 
 import os
+import yaml
+
+import numpy as np
 
 from oracle.specutils import Spectrum1D
 import transitions
 #from oracle.models import transitions
 
+
+
+def minimum_pixel_sampling(data, wavelength):
+    """
+    Find the minimum pixel size in the data at the given wavelength.
+    """
+
+    pixel_size = np.nan
+    channel_indices = []
+    for i, spectrum in enumerate(data):
+        if spectrum.disp[-1] >= wavelength >= spectrum.disp[0]:
+            channel_indices.append(i)
+
+            index = spectrum.disp.searchsorted(wavelength)
+
+            diff = np.diff(spectrum.disp)
+            if pixel_size > diff[index] or not np.isfinite(pixel_size):
+                pixel_size = diff[index]
+
+    if len(channel_indices) == 0 and not np.isfinite(pixel_size):
+        raise ValueError("cannot find wavelength {0:.2f} in any data channel"\
+            .format(wavelength))
+
+    return (pixel_size, channel_indices)
+
+
 def atomic_transition():
 
-    data = Spectrum1D.load("Spectra/ccd_3/sun_red.0002.fits")
-    atomic_data = {
-        "wavelength": 6609.110,
-        "species": 26.0,
-        "excitation_potential": 2.557,
-        "loggf": -2.692,
-        "blending_transitions": [
-            [6607.652, 24.0, 5.788, -3.784],
-            [6607.675, 24.0, 3.365, -4.181],
-            [6607.818, 23.0, 1.349, -1.960],
-            [6608.025, 26.0, 2.277, -4.030],
-            [6608.198, 57.0, 1.234, -0.780],
-            [6608.434, 23.0, 4.305, -2.866],
-            [6608.473, 21.0, 3.875, -2.298],
-            [6608.626, 24.1, 6.482, -3.089],
-            [6608.793, 26.0, 5.326, -4.209],
-            [6608.936, 24.0, 4.398, -1.048],
-            [6608.952, 26.0, 4.188, -2.781],
-            [6609.032, 74.0, 1.915, -2.530],
-            [6609.234, 26.1, 7.123, -3.022],
-            [6609.252, 20.0, 5.736, -5.852],
-            [6609.255, 25.1, 6.860, -2.053],
-            [6609.678, 26.0, 0.989, -5.002],
-            [6609.985, 20.0, 5.737, -4.014],
-            [6610.035, 64.1, 1.657, -1.786],
-            [6610.073, 20.0, 5.877, -5.517],
-            [6610.392, 26.0, 5.503, -2.864],
-            [6610.453, 25.0, 5.920, -1.386],
-            [6610.475, 21.1, 7.466, 0.325]
-        ]
-    }
+    datas = [
+        Spectrum1D.load("Spectra/ccd_1/sun_blu.0002.fits"),
+        Spectrum1D.load("Spectra/ccd_2/sun_grn.0002.fits"),
+        Spectrum1D.load("Spectra/ccd_3/sun_red.0002.fits"),
+        Spectrum1D.load("Spectra/ccd_4/sun_ir.0002.fits"),
+    ]
+    with open("hermes-atomic-linelist.yaml", "r") as fp:
+        model = yaml.load(fp)
+
+    atomic_transitions = model["model"]["atomic_transitions"]
+
+    # Pick one
+    i = np.random.randint(0, len(atomic_transitions))
+    atomic_data = atomic_transitions[i]
+    
+
+    ps, i = minimum_pixel_sampling(datas, atomic_data["wavelength"])
+    data = datas[i[0]]
 
     line = transitions.AtomicTransition(**atomic_data)
     print(line)
@@ -48,9 +64,20 @@ def atomic_transition():
         "metallicity": 0.,
         "microturbulence": 1.4
     }
-    line.fit_profile(data, initial_theta=initial_theta, continuum_order=1)
+    result = line.fit_profile(data, initial_theta=initial_theta,
+        continuum_order=1, full_output=True)
+
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    ax.plot(result[3].disp, result[3].flux, 'b')
+    ax.plot(data.disp, data.flux, 'k')
+    ax.set_xlim(result[3].disp[0], result[3].disp[-1])
+    plt.show()
 
     raise a
+
+
+    
 
 # Don't run this test on Travis.
 if "TRAVIS_BUILD_ID" not in os.environ:
