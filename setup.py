7b462e5f439b5b3996b4525eee3019853060c9ab
Andy Casey <andycasey@gmail.com>
2015-02-09 23:47:36 +0000

    Trying tests with travis

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-""" oracle, the suppository of wisdom """ 
+""" oracle, the suppository of all wisdom """ 
 
 from __future__ import division, absolute_import, print_function
 
@@ -9,24 +10,16 @@ __author__ = "Andy Casey <arc@ast.cam.ac.uk>"
 # Standard library.
 import os
 import re
-import subprocess
 import sys
-from glob import glob
 from urllib import urlretrieve
 
 # Third-party.
-from numpy.distutils.core import Command, Extension, setup
-
-#try:
-#    from setuptools import setup
-
-#except ImportError:
-#    from distutils.core import setup
+from numpy.distutils.core import Extension, setup
 
 major, minor1, minor2, release, serial =  sys.version_info
-open_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
 
 def readfile(filename):
+    open_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
     with open(filename, **open_kwargs) as fp:
         contents = fp.read()
     return contents
@@ -41,14 +34,14 @@ version = version_regex.findall(contents)[0]
 # Extensions.
 moog = Extension(name = "oracle.synthesis._mini_moog",
     sources = ["oracle/synthesis/source/moog/{}".format(each) for each in [
-        'MyAbfind.f', 'Partfn.f', 'Sunder.f', 'Eqlib.f', 'Nearly.f','Discov.f',
-        'Invert.f', 'Gammabark.f', 'Damping.f', 'Lineinfo.f', 'Opacit.f',
-        'Blankstring.f', 'Prinfo.f', 'Opacmetals.f', 'Synspec.f', 'Cdcalc.f',
-        'Linlimit.f', 'Taukap.f', 'Jexpint.f', 'Partnew.f', 'Opacscat.f',
-        'OpacHelium.f', 'OpacHydrogen.f', 'Opaccouls.f', 'Rinteg.f', 
-        'Trudamp.f', 'Ucalc.f', 'Voigt.f', 'Fakeline.f', 'Curve.f',
-        'Lineabund.f', 'Molquery.f', 'Oneline.f', 
-        'Inmodel.f', 'Inlines.f', 'Batom.f', 'Bmolec.f', 'MySynth.f']])
+        "MyAbfind.f", "Partfn.f", "Sunder.f", "Eqlib.f", "Nearly.f","Discov.f",
+        "Invert.f", "Gammabark.f", "Damping.f", "Lineinfo.f", "Opacit.f",
+        "Blankstring.f", "Prinfo.f", "Opacmetals.f", "Synspec.f", "Cdcalc.f",
+        "Linlimit.f", "Taukap.f", "Jexpint.f", "Partnew.f", "Opacscat.f",
+        "OpacHelium.f", "OpacHydrogen.f", "Opaccouls.f", "Rinteg.f", 
+        "Trudamp.f", "Ucalc.f", "Voigt.f", "Fakeline.f", "Curve.f",
+        "Lineabund.f", "Molquery.f", "Oneline.f", 
+        "Inmodel.f", "Inlines.f", "Batom.f", "Bmolec.f", "MySynth.f"]])
 
 # External data.
 if "--with-atmospheres" in map(str.lower, sys.argv):
@@ -67,27 +60,32 @@ if "--with-atmospheres" in map(str.lower, sys.argv):
                 "--with-atmospheres flag".format(url))
     sys.argv.remove("--with-atmospheres")
 
-
+# Now the magic.
 setup(
     name="oracle",
     version=version,
     author="Andrew R. Casey",
     author_email="arc@ast.cam.ac.uk",
-    packages=["oracle", "oracle.atmospheres", "oracle.models",
-        "oracle.specutils", "oracle.synthesis"],
+    packages=[
+        "oracle", "oracle.atmospheres", "oracle.models", "oracle.specutils",
+        "oracle.synthesis"
+    ],
     url="http://www.github.com/andycasey/oracle/",
     description="the suppository of all wisdom",
-    long_description=readfile(os.path.join(os.path.dirname(__file__), "README.md")),
+    long_description=readfile(
+        os.path.join(os.path.dirname(__file__), "README.md")),
     install_requires=readfile(
         os.path.join(os.path.dirname(__file__), "requirements.txt")).split("\n"),
     entry_points={
         "console_scripts": ["oracle = oracle.cli:main"]
     },
     ext_modules=[moog],
-    #scripts=["oracle/"],
     include_package_data=True,
     package_data={
-        "oracle.atmospheres": ["marcs-2011-standard.pkl", "castelli-kurucz-2004.pkl"],
+        "oracle.atmospheres": [
+            "marcs-2011-standard.pkl",
+            "castelli-kurucz-2004.pkl"
+        ],
         "oracle.models": ["galah-ambre-grid.pickle"]
     }
 )
